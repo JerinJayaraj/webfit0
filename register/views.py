7851bbe4d5d92eae60760f2277e331317e8cae33
jerin <4jerin4@gmail.com>
2019-07-25 00:05:35 +0530

    changed

--- register/views.py
+++ register/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User, auth
 
 # Create your views here.
 def home(request):
-    if request.method=='POST':
-        name1=Register.objects.all()
-        print(name1)
 
+    if request.method=='POST':
+       useranme = request.POST['username']
+       user= auth.authenticate(usernme=username)
+       return render(request, 'a.html')
     else:
         return render(request, 'home.html')
